888f39ed97d0db2f7a4a129af557e24c3372a8e0
Midhet Sulemani <m.sulemani@yabila.com>
2019-11-11 17:47:07 +0300

    initial commit

---
+++ myapi/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from .serializers import HeroSerializer
+from .models import Hero
+
+# Create your views here.
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('name')
+    serializer_class = HeroSerializer
